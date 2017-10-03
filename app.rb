177bc027fa465c2e571d315c9e5d4dcbe0cd4b88
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2017-10-03 13:13:20 -0700

    Use SSE to do live updates on the clients

--- app.rb
+++ app.rb
@@ -2,8 +2,7 @@ require 'sinatra'
 require 'json'
 require_relative 'node'
 
-set :public_folder, 'public'
-set :port, ENV['PORT']
+set public_folder: 'public', port: (ENV['PORT'] || 4567), server: 'thin', connections: []
 
 NODE = Node.new
 
@@ -17,6 +16,7 @@ end
 
 post '/transactions' do
   NODE.add_transaction params[:from], params[:to], params[:amount].to_i, params[:id]
+  settings.connections.each { |out| out << "data: added transaction\n\n" }
   redirect '/'
 end
 
@@ -26,10 +26,22 @@ post '/mine' do
 end
 
 post '/resolve' do
-  NODE.resolve(JSON.parse(request.body.read)) ? status(202) : status(200)
+  if NODE.resolve(JSON.parse(request.body.read)) 
+    status 202
+    settings.connections.each { |out| out << "data: resolved\n\n" }
+  else 
+    status 200
+  end
 end
 
 post '/peers' do
   NODE.add_peer params[:address]
   redirect '/'
+end
+
+get '/events', provides: 'text/event-stream' do
+  stream :keep_open do |out|
+    settings.connections << out
+    out.callback { settings.connections.delete(out) }
+  end
 end
